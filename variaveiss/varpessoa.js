7b1c0b1ef39d0a2f4f906910682524c9b1cbadbb
kauegabrielpereiranascimento-art <kauegabrielpereiranascimento@gmail.com>
2026-02-25 13:28:27 +0000

    fizemos variaveis para pessoas

---
+++ variaveiss/varpessoa.js
@@ -0,0 +1,18 @@
+//, var, let, const
+// tipos de dados
+
+let nome = "Kaue"         //sting
+let sobrenome = "Gabriel"  //sting
+const cpf = "22222222-62"   //string
+const rg = "132131 ssp"    //string
+let idade = 16             //number
+let solteiro = true       //boolean
+let nota = 5 + 3          //number 
+ 
+console.log(nome)
+console.log(sobrenome)
+console.log(cpf)
+console.log(rg)
+console.log(idade)
+console.log(solteiro)
+console.log(nota)
