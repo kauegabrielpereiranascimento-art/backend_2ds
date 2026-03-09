3890e3021c836f44c34c2cf2a8a389529a0f9d48
kauegabrielpereiranascimento-art <kauegabrielpereiranascimento@gmail.com>
2026-03-09 13:26:21 +0000

    introdução aos Arrays em JS

---
+++ condicionais/array/array-time.js
@@ -0,0 +1,10 @@
+//Array: são agrupamentos de valores em forma de list
+//São indexados a partir de ZERO   até o infinito
+//Podem ter valores homegênos/heterogênos
+//Possuam métodos (funções) próprios
+
+const time = ['Petter', 'Mariah']
+  // indeices    0         1   
+
+console.log(time[1])
+console.log(time[32])//undefined
---
+++ condicionais/if-else.js
@@ -0,0 +1,17 @@
+// if, else if, else, ternário
+// >, <, >=, <=, !=
+// || &&
+
+let idade = 111
+
+if (idade >= 110) {
+    console.log("Situação ímpar")
+    console.log("Traga um acompanhante")
+} else if(idade >=17 && idade <=110 ){
+     console.log("Entrada Liberada")
+     console.log("Festa sugerida: Balada!!!")
+} else{
+  console.log("Proibido, vaza menor!")
+  console.log("Festa recomenda: Monica Toy")
+  
+}
