86c832760a7d12c4d717d9ba015cb36e388a4dd6
kauegabrielpereiranascimento-art <kauegabrielpereiranascimento@gmail.com>
2026-03-25 11:42:48 +0000

    finalização de Obijetos.JS

--- objetos/obj-02.js
+++ objetos/obj-02.js
@@ -2,13 +2,13 @@ const carro = {
     modelo: "Opala",
     marca: "Chevrolet",
     cor: "preto",
-    motor: V8,
+    motor: "V8",
     ano: 1972,
     litros: 55,
 }
-console.log(""+carro.modelo)
-console.log(""+carro.marca)
-console.log(""+carro.cor)
-console.log(""+carro.motor)
-console.log(""+carro.ano)
-console.log(""+carro.litros)
+console.log("modelo: " + carro.modelo);
+console.log("marca: " + carro.marca);
+console.log("cor: " + carro.cor);
+console.log("motor: " + carro.motor);
+console.log("ano: " + carro.ano);
+console.log("litros: " + carro.litros);
