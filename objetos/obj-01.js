fd521b9a4a5a3043452ac8f23b7ea59ad4092b02
kauegabrielpereiranascimento-art <kauegabrielpereiranascimento@gmail.com>
2026-03-25 11:37:13 +0000

    Finalização de Objetos.JS

---
+++ objetos/obj-01.js
@@ -0,0 +1,19 @@
+// Objetos são estruturas chave-valor
+// Também chamadas "Key-value"
+// number, objects, functions...
+//possuem métodos próprios
+
+const jogador = {
+    nome:"Kaue",
+    defesas: 257,
+    penaltis: 57,
+    idade: 15,
+    peso: 70,
+    time: ['Atlético Peñarol', 'Flamengo', 'PSG' ]
+}
+console.log(jogador.nome)
+console.log(jogador.defesas)
+console.log(jogador.penaltis)
+console.log(jogador.idade)
+console.log(jogador.peso)
+console.log(jogador.time[0])
---
+++ objetos/obj-02.js
@@ -0,0 +1,14 @@
+const carro = {
+    modelo: "Opala",
+    marca: "Chevrolet",
+    cor: "preto",
+    motor: V8,
+    ano: 1972,
+    litros: 55,
+}
+console.log(""+carro.modelo)
+console.log(""+carro.marca)
+console.log(""+carro.cor)
+console.log(""+carro.motor)
+console.log(""+carro.ano)
+console.log(""+carro.litros)
