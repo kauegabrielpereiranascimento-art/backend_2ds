76db9699ddace0f0dbeb98ac2e33c7c5d97315c0
kauegabrielpereiranascimento-art <kauegabrielpereiranascimento@gmail.com>
2026-05-27 10:51:11 +0000

    Finalização module ESM-Petshop

---
+++ 2º TRIMESTRE/modulos/mod02-ESM/gato-mod.js
@@ -0,0 +1,14 @@
+//USA PADRÃO MODERNO IMPORT/EXPORT - ESM
+//Aqui criaremos nossos móduloos
+
+export function banhoGato(raça, tipoBanho)
+{
+    return `O gato ${raça} tomou banho: ${tipoBanho}`
+}export function tosaGato(raça, tipoTosa)
+{
+    return `O gato ${raça} fey tosa: ${tipoTosa}`
+}
+export function alimentarGato(raça, tipoComida)
+{
+    return `O gato ${raça} comeu: ${tipoComida}`
+}
---
+++ 2º TRIMESTRE/modulos/mod02-ESM/main-gato-mod.js
@@ -0,0 +1,8 @@
+//APENAS IMPORTA MÓDULOS DE 
+// OUTROS ARQUIVOS (IMPORT)
+import {alimentarGato, tosaGato, banhoGato} from './gato-mod.js'
+
+console.log(alimentarGato('Angorá', 'Petisco'))
+console.log(banhoGato('Seamês', 'Completo'))
+console.log(tosaGato('Persa', 'Bem Baixinha'))
+
