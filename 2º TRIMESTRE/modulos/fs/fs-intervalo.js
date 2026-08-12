90bc1e0aab079ca03fa7cb388ec85a37424315dc
kauegabrielpereiranascimento-art <kauegabrielpereiranascimento@gmail.com>
2026-08-12 11:49:47 +0000

    Adicionar arquivos de página inicial e sobre, e implementar servidor HTTP

---
+++ 2º TRIMESTRE/modulos/fs/fs-intervalo.js
@@ -0,0 +1,8 @@
+// seTimeout(ms) - executa 1 vez
+setTimeout(function(){
+ console.log('Exectou timeout...')
+}, 3000)
+// setInterval(ms) - executa 'x' vezes
+setInterval(function(){
+    console.log('Executar FPS 30')
+}, 2000)
